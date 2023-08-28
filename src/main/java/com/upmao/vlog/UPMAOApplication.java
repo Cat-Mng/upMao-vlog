8dca69610f4a8d7036a0c54c0e9a763efb879529
Cat-Mng <supermao2022@163.com>
2023-08-28 11:08:13 +0800

    create template

--- src/main/java/com/upmao/vlog/UPMAOApplication.java
+++ src/main/java/com/upmao/vlog/UPMAOApplication.java
@@ -1,7 +1,12 @@
 package com.upmao.vlog;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UPMAOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UPMAOApplication.class);
+    }
 }
